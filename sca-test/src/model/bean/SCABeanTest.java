e430709adad472fb5e7ccd298403445c84b153f4
avianabr <alexandre.viana.br@gmail.com>
2018-05-23 10:00:57 -0300

    commit

--- sca-test/src/model/bean/SCABeanTest.java
+++ sca-test/src/model/bean/SCABeanTest.java
@@ -12,14 +12,17 @@ public class SCABeanTest {
 
 		Hashtable ht = new Hashtable();
 		ht.put(Context.INITIAL_CONTEXT_FACTORY, "weblogic.jndi.WLInitialContextFactory");
-		ht.put(Context.PROVIDER_URL, "t3://localhost:7001");
+		//ht.put(Context.PROVIDER_URL, "t3://localhost:7003,localhost:7004,localhost:7005");
+		ht.put(Context.PROVIDER_URL, args[0]);
+		
+		
 
 		try {
 			ctx = new InitialContext(ht);
 			// Use the context in your program
 			SCABeanRemote scaBean = null;
 			scaBean = (SCABeanRemote) ctx.lookup("sca-ear-sca-ejb-SCABean#model.bean.SCABeanRemote");
-			scaBean.criarUsuario(new Long(1), "Alexandre Viana", "aviana", "Welcome1");
+			scaBean.criarUsuario(new Long(args[1]), args[2], args[3], args[4]);
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
